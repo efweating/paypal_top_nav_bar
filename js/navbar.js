402ee18f85e0d75baee20190a69e947143bd49d9
efweating <56888666+efweating@users.noreply.github.com>
2021-08-23 13:59:48 -0400

    Updated Navigation Bar

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,28 @@
-$(document)
-.ready(function() {
+window.onload = function() {
 
-  $('.main.menu  .ui.dropdown').dropdown({
-    on: 'hover'
-  });
-})
-;
+let dropdowns = document.getElementsByClassName("dropdown");
+
+
+for (let i = 0; i < dropdowns.length; i++) {
+  let dropdown = dropdowns[i];
+
+  dropdown.addEventListener("mouseenter", function( event ) {
+    event.target.style.color = "purple";
+
+   setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  }, false);
+
+  dropdown.addEventListener("mouseover", function( event ) {
+    event.target.style.color = "red";
+
+   setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  }, false);
+
+}
+  //debugger;
+}
+
